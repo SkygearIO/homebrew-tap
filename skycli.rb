277360266537543fd5e752a03e4dfe82d8528662
Kwok-kuen Cheung <me@cheungpat.com>
2017-09-28 18:36:42 +0800

    skycli 0.1.1-2 (#3)

--- skycli.rb
+++ skycli.rb
@@ -3,10 +3,10 @@ require "language/node"
 class Skycli < Formula
   desc "Skygear Portal command line tool"
   homepage "https://skygear.io"
-  url "https://registry.npmjs.org/skycli/-/skycli-0.1.1-1.tgz"
-  sha256 "9a15e905bd1c1136653c54d494af877a5299317afbbce2c32b625260569fd909"
+  url "https://registry.npmjs.org/skycli/-/skycli-0.1.1-2.tgz"
+  sha256 "4aa7eae9c22f69375d31623335ec35a48eb7249728f12804c48e1bab45e13251"
   head "https://github.com/SkygearIO/skycli.git"
-  version "0.1.1-1"
+  version "0.1.1-2"
 
   depends_on "node"
 
