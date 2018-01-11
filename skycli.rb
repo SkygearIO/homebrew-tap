db21ce8840418e1a370bae4b2f4d00792fb1c19e
Kwok-kuen Cheung <me@cheungpat.com>
2018-01-11 14:30:31 +0800

    Fix skycli 0.4.0 sha

--- skycli.rb
+++ skycli.rb
@@ -4,7 +4,7 @@ class Skycli < Formula
   desc "Skygear Portal command line tool"
   homepage "https://skygear.io"
   url "https://registry.npmjs.org/skycli/-/skycli-0.4.0.tgz"
-  sha256 "aed9496a4adbe9a866a4ad547f8329ba5fdd93cb2ada1f7c3eddf308b"
+  sha256 "aed9496a4adbe9a866a4ad547f8329ba5fdd93cb2ada1f7c3eddf308b89c754c"
   head "https://github.com/SkygearIO/skycli.git"
   version "0.4.0"
 
